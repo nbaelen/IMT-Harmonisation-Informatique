a72b6feb9af53dee6b11978572dacf9765c0a12c
nbaelen <n.baelen@gmail.com>
2019-10-23 11:05:34 +0200

    pascal.c deuxième version

--- pascal.c
+++ pascal.c
@@ -4,22 +4,17 @@
 #include "stdio.h"
 
 #define MAX 10
-int ligne[MAX], ligneSup[MAX];
-int i, j;
+int ligne[MAX];
+int i;
 
 void pascal() {
-    for (i = 0; i < MAX; ++i) ligneSup[i] = 0;
-    ligneSup[0] = 1;
+    for (i = 0; i < MAX; ++i) ligne[i] = 0;
+    ligne[0] = 1;
 
     for (int k = 0; k < 7; ++k) {
-        for (i = 0; i < MAX; i++) ligne[i] = ligneSup[i] + ligneSup[i - 1];
+        for (i = 0; i < MAX; i++) if (ligne[i] != 0) printf("%d ", ligne[i]);
+        ligne[0] = 1;
+        for (i = MAX; i > 0; i--) ligne[i] += ligne[i - 1];
         printf("\n");
-
-        for (i = 0; i < MAX; i++) {
-            if (ligneSup[i] != 0) printf("%d ", ligneSup[i]);
-            ligneSup[i] = ligne[i];
-        }
     }
-
-
 }
